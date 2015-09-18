326ed03ccc5b32fc7d990eac711d483a9f74eaae
fvi-att <shimizu.r.ad@m.titech.ac.jp>
2015-09-19 01:24:52 +0900

    remove file no longer used

--- install.el
+++
@@ -1,8 +0,0 @@
-;;; install emacs plugin
-;;; AUTHOR fvi@ <jshimizujp@gmail.com>
-;;; VERSION 0.5
-
-(package-refresh-contents)
-(package-install auto-complete)
-(package-refresh-contents)
-(package-install undohist)
